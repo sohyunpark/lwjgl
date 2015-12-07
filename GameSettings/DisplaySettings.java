c6f665b956d32646f8402aba8936a27ccb647b83
sohyunpark <sohyun308@naver.com>
2015-12-07 17:32:23 +0900

    2st commit

---
+++ GameSettings/DisplaySettings.java
@@ -0,0 +1,16 @@
+package GameSettings;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayImplementation;
+import org.lwjgl.opengl.Displaymode; 
+
+
+public class DisplaySettings {
+	 
+	public static int GraphicSpeed(){}
+	public static boolean isLight(){}
+	public static boolean is3DScreen(){}
+	public static boolean isFlick(){}
+	public static String ParticleSize(){}
+	public static String FieldofVision(){}
+	
+}
---
+++ GameSettings/SoundSetting.java
@@ -0,0 +1,17 @@
+package GameSettings;
+import org.lwjgl.openal.ALC11;
+import org.lwjgl.openal.ALCcontext;
+import org.lwjgl.openal.ALCdevice;
+
+
+public class SoundSetting {
+	private static void initialize(){}
+	public static boolean isKeyEvent(){} 
+	public static boolean isCursorkeyDown(int key){} 
+	public static long getCursorEventState(){}
+	public static long getEventState(){}
+	public static synchronized int getKeyIndex(String keyName){} 
+	public static boolean next(){}
+	private static void create(InputImplementation impl) throws LWJGLException{}
+
+}
